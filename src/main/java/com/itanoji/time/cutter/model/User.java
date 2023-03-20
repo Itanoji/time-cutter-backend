79e398120aba68df563559733e591f7ffb81cc1c
Amadeus <igniferwargus@gmail.com>
2023-03-21 00:49:42 +0300

    added database

---
+++ src/main/java/com/itanoji/time/cutter/model/User.java
@@ -0,0 +1,24 @@
+package com.itanoji.time.cutter.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    @Column(unique=true)
+    private String login;
+    private String password;
+    @Column(unique=true)
+    private String email;
+
+}
