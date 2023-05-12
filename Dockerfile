ae16072c58b37e3562c2c68db20806917366e4d0
Amadeus <igniferwargus@gmail.com>
2023-05-12 20:04:00 +0300

    Обновил докер

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
-FROM openjdk:19
+FROM maven:3.8.2-openjdk-17 AS build
 WORKDIR /app
-COPY .mvn/ .mvn
-COPY mvnw pom.xml ./
-RUN ./mvnw dependency:go-offline
+COPY pom.xml .
+RUN mvn dependency:go-offline
 COPY src ./src
-RUN ./mvnw package -DskipTests
-CMD ["java", "-jar", "target/time.cutter-1.0.0.jar"]
+RUN mvn package -DskipTests
+
+FROM openjdk:17
+WORKDIR /app
+COPY --from=build /app/target/time.cutter-1.0.0.jar ./
+CMD ["java", "-jar", "time.cutter-1.0.0.jar"]
--- src/main/java/com/itanoji/time/cutter/config/WebConfig.java
+++ src/main/java/com/itanoji/time/cutter/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Authorization")
