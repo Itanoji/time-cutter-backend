1a8fed8af149165658ef97d03bf3e2b5c13defcd
Amadeus <igniferwargus@gmail.com>
2023-05-17 01:37:08 +0300

    CORS fixed

--- src/main/java/com/itanoji/time/cutter/config/WebConfig.java
+++ src/main/java/com/itanoji/time/cutter/config/WebConfig.java
@@ -10,12 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Authorization", "Content-Type")
-                .exposedHeaders("Authorization")
-                .allowCredentials(true)
-                .maxAge(3600);
+        registry.addMapping("/api/**");
     }
 }
